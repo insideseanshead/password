658701af1fccac5d38f718bedf723850af97e43d
Sean Morgan <insideseanshead@gmail.com>
2020-12-08 12:16:56 -0800

    refactored number generator into array of numbers 1 through 9, added conditional to check that password is appropriate length, added confirms for all values and pushed arrays if confirm is true

--- script.js
+++ script.js
@@ -20,42 +20,49 @@ let passValues = [];
 
 // add variables for uppercase and lowercase 
 const upperCase= [] = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ").split("")
-const lowerCase= [] = ("abcdefghijklmnopwqrstuvwxyz").split("")
+const lowerCase= [] = ("abcdefghijklmnopqrstuvwxyz").split("")
 
 // array of random special characters
 const specialChar = [] = ("!@#$%&?+-*").split("")
 
 
-// function to generate random number.
-function randomNum() {
-  num=Math.floor(Math.random()*10)
-  console.log(num)
-}
+// array of numbers 1 to 10.
+const numbers = [1,2,3,4,5,6,7,8,9]
 
 randomNum()
 console.log(upperCase,lowerCase,specialChar)
 function generatePassword() {
   let passLength = prompt("How many characters would you like your password to be?")
   // check pass length between 12 & 72 characters in length
-
+  if(passLength < 12 || passLength > 72){
+    alert("please pick a number between 12 and 72")
+  } else {
   //TODO: confirm if user wants Uppercase Letters
-
+    let upperCaseSelect = confirm("Would you like your password to contain uppercase letters?")
   //TODO: if true push upperCase array to passValues Array
-
+    if(upperCaseSelect){
+      passValues.push(upperCase)
+    }
   //TODO: confirm if user wants lowercase Letters
-
+    let lowerCaseSelect = confirm("Would you like your password to contain lowercase letters?")
   //TODO: if true push lowercase array to passValues Array
-
+    if(lowerCaseSelect){
+      passValues.push(lowerCase)
+    }
   //TODO: confirm if user wants specialChar
-
+    let spectialSelect = confirm("Would you like your password to contain special characters?")
   //TODO: if true push specialChar array to passValues Array
-
+    if(spectialSelect){
+      passValues.push(specialChar)
+    }
   //TODO: confirm if user wants numbers
-
+    let numSelect = confirm("Would you like your password to contain numbers?")
   //TODO: if true push numbers array to passValues Array
-
+    if(numSelect){
+      passValues.push(numbers)
+    }
   //TODO: Check to make sure user added at least one type of character
-
+    
   //TODO: use loop to pull random characters from passValues Array
 
   //TODO: Make sure given password has at least one of each selected character
@@ -63,5 +70,6 @@ function generatePassword() {
   //TODO: If password does not meet condition create new password from pre selected optopns until conditions are met.
 
   //TODO: If password meets all conditions return password
+  }
 }
 
